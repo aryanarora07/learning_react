e9fcc211438f99425598a8ae4a1bc8d070c62b48
aryanarora07 <aroraaryan824@gmail.com>
2024-01-24 20:51:42 -0800

    connecting frontend and backend

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,7 +1,12 @@
-export function Todos(){
+export function Todos({todos}){
     return <div>
-        <h1>go to the gym</h1>
-        <h2>You need to go to the gym</h2>
-        <button>Mark as Completed</button>
+        {todos.map(function(todo){
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+
+                <button>{todo.completed == true ? "Completed" :  "Mark as Complete"}</button>
+                </div>
+        })}
     </div>
 }
